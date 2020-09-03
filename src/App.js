a3ec84d00088e6e3cc1daf9406a47c870f8429ad
Mathias <mathiasjtaylor@gmail.com>
2020-09-03 10:08:53 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import './App.css';
-import {FuncCardSet} from 'c:/Spring_Workspace/components/src/Components/FuncCardSet';
+
 
 function App() {
   return (
     <div className="App">
-      <FuncCardSet/>
+      <p>Welcome</p>
     </div>
   );
 }
